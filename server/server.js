5ad4c5f29c8d89a409df623864149ec18a0aa8ac
Adrian <adde15w@hotmail.com>
2018-04-10 16:15:47 +0200

    Added newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -7,12 +7,22 @@ const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
-var io = socketIO(server);          // websocket server
+var io = socketIO(server);          // websocket server - server to client
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
+
+    socket.emit('newMessage', {
+        from: 'adde@mail.com',
+        text: 'Hey, its me lets ut lunch',
+        createdAt: '17:15'
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('Createmessage', message);
+    });
 
     socket.on('disconnect', () => {
         console.log('Client disconnected...');
